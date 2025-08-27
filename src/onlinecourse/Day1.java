fd346c559de46e9a1a2f54721055139f4b40594c
Nagajyothi969 <nagajyothigurrala905@gmail.com>
2025-08-27 10:00:11 +0530

    added

---
+++ src/onlinecourse/Day1.java
@@ -0,0 +1,17 @@
+package onlinecourse;
+
+import java.util.Scanner;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int add = a + b;
+		System.out.println(" sum is equal to" + add);
+		
+sc.close();
+	}
+
+}
